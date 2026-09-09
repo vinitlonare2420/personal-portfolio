6df09a6947f87786fe730a972a082043a83989f0
vinitlonae2420 <vinitlonare0@gmail.com>
2026-09-09 21:14:29 +0530

    Implement project filtering

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// JavaScript will be added as the project grows.
+// Project Filtering
+var filterButtons = document.querySelectorAll(".filter-button");
+var projectCards = document.querySelectorAll(".project-card");
+
+filterButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var selectedCategory = button.getAttribute("data-filter");
+
+    projectCards.forEach(function (project) {
+      var projectCategory = project.getAttribute("data-category");
+
+      if (selectedCategory === "all") {
+        project.classList.remove("hidden");
+      } else if (selectedCategory === projectCategory) {
+        project.classList.remove("hidden");
+      } else {
+        project.classList.add("hidden");
+      }
+    });
+  });
+});
